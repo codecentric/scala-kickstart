a36cac8af1e0a9b68d2ee73c5a45faa43e1d2dd4
Benedikt Ritter <beneritter@gmail.com>
2016-07-08 13:48:52 +0200

    Example for def, var and val

--- src/main/scala/de/codecentric/scalakickoff/App.scala
+++ src/main/scala/de/codecentric/scalakickoff/App.scala
@@ -2,7 +2,16 @@ package de.codecentric.scalakickoff
 
 object App {
 
+  // in java: public final int number = 5  
+  val number = 5
+
+  // in java: public int mutableNum = 5
+  var mutableNum = 5
+  
+  // in java: public int giveNumber { return 5 }
+  def giveNumber = 5
+
   def main(args: Array[String]) {
-    println("Hello from Scala")
+    println(s"Hello from Scala $number")
   }
 }
